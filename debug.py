d8e785fc39dd421c63b1c66ec1f18f357603d80c
Keith Trnka <keith.trnka@gmail.com>
2016-04-20 20:46:30 -0700

    Added checker between X_train and X_test, wrapper to run it with cross-validation, and helper to run cross-validation and return the predictions.

--- debug.py
+++ debug.py
@@ -12,7 +12,10 @@ import sklearn.linear_model
 import sklearn.cross_validation
 from sklearn.metrics import mean_absolute_error
 import sklearn.ensemble
-
+import sklearn.datasets
+import sklearn.utils.validation
+import sklearn.base
+import sklearn.metrics.scorer
 
 def get_input_gradient(y_true, y_pred, x, model, eps=1e-4, param_eps=None, max_eps=20.):
     """Approximate numeric gradient on this one example with respect to the inputs (assumes the model weights are fixed)"""
@@ -53,6 +56,69 @@ def get_input_gradient(y_true, y_pred, x, model, eps=1e-4, param_eps=None, max_e
     return input_gradients
 
 
+def compute_input_fairness(data_train, data_test, verbose=0):
+    assert isinstance(data_train, numpy.ndarray)
+    assert isinstance(data_test, numpy.ndarray)
+
+    mean = data_train.mean(axis=0)
+    std = data_train.std(axis=0)
+
+    z_scores = (data_test - mean) / std
+    mean_z_scores = z_scores.mean(axis=0)
+
+    abs_z_scores = abs(z_scores)
+    mean_abs_z_scores = abs_z_scores.mean(axis=0)
+
+    if verbose:
+        print("Average abs z-score of test w.r.t train: ", mean_abs_z_scores.mean())
+
+    # compute percent out of some range like 5 sigma
+    outliers = abs(z_scores) > 5
+    outlier_percents = outliers.mean(axis=0)
+
+    if verbose:
+        for i, (feature_percentage, z_score, abs_z) in enumerate(zip(outlier_percents, mean_z_scores, mean_abs_z_scores)):
+            print("Feature {}: {:.1f}% outliers, z: {:.3f}, abs(z): {:.3f}".format(i, 100. * feature_percentage, z_score, abs_z))
+
+    return mean_abs_z_scores.mean(), max(mean_abs_z_scores), max(outlier_percents)
+
+
+def compute_cross_validation_fairness(X, Y, splits):
+    ### TODO: Test this code. It's 100% untested!
+    for i, (train, test) in enumerate(splits):
+        print("Checking fairness of CV split {}".format(i))
+
+        mean_mean_abs_z, max_mean_abs_z, max_outliers = compute_input_fairness(X[train], X[test])
+        if mean_mean_abs_z > 1. or max_mean_abs_z > 1. or max_outliers > 0.05:
+            print("X{test] doesn't match X[train] well", mean_mean_abs_z, max_mean_abs_z, max_outliers)
+            compute_input_fairness(X[train], X[test], verbose=1)
+
+        mean_mean_abs_z, max_mean_abs_z, max_outliers = compute_input_fairness(Y[train], Y[test])
+        if mean_mean_abs_z > 1. or max_mean_abs_z > 1. or max_outliers > 0.05:
+            print("Y{test] doesn't match Y[train] well", mean_mean_abs_z, max_mean_abs_z, max_outliers)
+            compute_input_fairness(Y[train], Y[test], verbose=1)
+
+
+def cross_val_score(estimator, X, y=None, scoring=None, cv=None):
+    """Run cross-validation like normal but return (scores, predictions)"""
+    ### TODO: Test this code. It's 100% untested!
+    X, y = sklearn.utils.validation.indexable(X, y)
+
+    cv = sklearn.cross_validation.check_cv(cv, X, y, classifier=sklearn.base.is_classifier(estimator))
+    scorer = sklearn.metrics.scorer.check_scoring(estimator, scoring=scoring)
+
+    y_pred = numpy.zeros_like(y)
+
+    scores = []
+    for train, test in cv:
+        current_est = sklearn.base.clone(estimator).fit(X[train], y[train])
+        predictions = current_est.predict(X[test])
+        scores.append(scorer(y[test], predictions))
+        y_pred[test] = predictions
+
+    return numpy.asarray(scores), y_pred
+
+
 def explain_input_gradient(input_gradient):
     for i, derivative in enumerate(input_gradient):
         print("To get desired prediction, the model wants you to adjust feature {} by {}".format(i, -derivative))
@@ -71,6 +137,22 @@ def stack_all_perturbations(x, perturb_all, perturb_elements=None, dtype=numpy.f
         perturb_matrix += numpy.diag(perturb_elements)
 
     return perturbations * perturb_matrix
+
+
+class EvaluationTests(unittest.TestCase):
+    def test_fairness(self):
+        X, y = sklearn.datasets.make_regression(n_samples=5000, n_features=20)
+        X_train, X_test = sklearn.cross_validation.train_test_split(X, train_size=0.2)
+        mean_z, max_abs_z, max_outlier_fraction = compute_input_fairness(X_train, X_test)
+        self.assertLess(mean_z, 1.)
+        self.assertLess(max_abs_z, 1.)
+        self.assertLess(max_outlier_fraction, 0.01)
+
+        # test on differently distributed data
+        mean_z, max_abs_z, max_outlier_fraction = compute_input_fairness(X, X + 20 * numpy.random.rand(*X.shape) - 10)
+        self.assertGreater(mean_z, 2.)
+        self.assertGreater(max_abs_z, 2.)
+        self.assertGreater(max_outlier_fraction, 0.1)
 
 
 class ExplanationTests(unittest.TestCase):
